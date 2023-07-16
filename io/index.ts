0702e0412516be0d5bed1efec9a5dfe1f1fe1352
Shariany Ibarra <shariany97@gmail.com>
2023-07-16 16:07:37 -0700

    Allow any origin with CORS

--- io/index.ts
+++ io/index.ts
@@ -3,7 +3,11 @@ import { Socket, Server } from "socket.io";
 
 export default (nuxtServer: NuxtServer) => {
   if (nuxtServer) {
-    const io = new Server(nuxtServer);
+    const io = new Server(nuxtServer, {
+      cors: {
+        origin: "*"
+      }
+    });
     io.on("connection", (socket: Socket) => {
       socket.emit("message", {
         message: "Server connected to Socket: " + socket.id + " :)",
